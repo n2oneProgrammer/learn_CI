2dcdc7038e1164549240bf4095619d9c53c4df3e
n2oneProgrammer <dominikwojtasik191@gmail.com>
2021-11-26 00:40:29 +0100

    commit7

--- tests/sum_test.py
+++ tests/sum_test.py
@@ -8,7 +8,7 @@ class SumTest(unittest.TestCase):
     def test_sum_correct1(self):
         self.assertEqual(sum_func(1, 2), 3)
 
-    def test_sum_correct2( self):
+    def test_sum_correct2(self):
         self.assertEqual(sum_func(10, 20), 30)
 
     def test_sum_correct3(self):
