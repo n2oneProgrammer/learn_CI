3320b05f92c862f184f93e6e804027fac7f1b5d8
n2oneProgrammer <dominikwojtasik191@gmail.com>
2021-11-26 00:36:51 +0100

    commit6

--- tests/sum_test.py
+++ tests/sum_test.py
@@ -6,10 +6,10 @@ from main import sum_func
 class SumTest(unittest.TestCase):
 
     def test_sum_correct1(self):
-        self.assertTrue(sum_func(1, 2), 3)
+        self.assertEqual(sum_func(1, 2), 3)
 
     def test_sum_correct2(self):
-        self.assertTrue(sum_func(10, 20), 30)
+        self.assertEqual(sum_func(10, 20), 30)
 
     def test_sum_correct3(self):
-        self.assertTrue(sum_func(11, 4), 15)
+        self.assertEqual(sum_func(11, 4), 15)
