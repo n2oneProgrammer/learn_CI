3610f4c744d92a91031012bb10c2cff9782b0882
n2oneProgrammer <dominikwojtasik191@gmail.com>
2021-11-25 23:52:09 +0100

    commit1

---
+++ main.py
@@ -0,0 +1,8 @@
+def sum_func(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    a = int(input("a: "))
+    b = int(input("b: "))
+    print(sum_func(a, b))
---
+++ tests/sum_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from main import sum_func
+
+
+class SumTest(unittest.TestCase):
+    def test_sum_correct1(self):
+        self.assertTrue(sum_func(1, 2), 3)
+
+    def test_sum_correct2(self):
+        self.assertTrue(sum_func(10, 20), 30)
+
+    def test_sum_correct3(self):
+        self.assertTrue(sum_func(11, 4), 15)
